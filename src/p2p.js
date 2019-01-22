4fff82c15191d0b64a779589a0bfd3e1c9889ff6
tndyd5390 <tndyd5390@naver.com>
2019-01-22 14:29:35 +0900

    자잘한 에러 수정

--- src/p2p.js
+++ src/p2p.js
@@ -79,10 +79,10 @@ const initSocketConnection = ws => {
     handleSocketError(ws);
     sendMessage(ws, getLatest());
     setTimeout(() => {
-        sendMessageToAll(ws, getAllMempool());
+        sendMessageToAll(getAllMempool());
     }, 1000);
     setInterval(() => {
-        if(sockets.includes(we)){
+        if(sockets.includes(ws)){
             sendMessage(ws, "");
         }
     }, 1000);
